c5ea204f8b9790b70177bcc688122f364c507726
Mathias Eklund <mathiaseklund@hotmail.se>
2014-08-21 07:20:47 +0200

    storing of items implemented

--- Bank/src/me/dojmgubben/bank/Methods.java
+++ Bank/src/me/dojmgubben/bank/Methods.java
@@ -25,8 +25,9 @@ public class Methods {
 		return instance;
 	}
 
-	public void enterBank(Player player, int i) {
-		int size = 9 * i;
+	public void enterBank(Player player, int in) {
+		int size = 9 * in;
+		String id = player.getUniqueId().toString();
 		Inventory inv = Bukkit.getServer().createInventory(player, size, player.getName() + "'s Bank");
 		ItemStack gold = new ItemStack(Material.getMaterial(plugin.config.getString("bank.gold.item")));
 		if (gold.getType() == Material.WOOL) {
@@ -36,7 +37,23 @@ public class Methods {
 		ItemMeta gm = gold.getItemMeta();
 		gm.setDisplayName(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bank.gold.name")));
 		gold.setItemMeta(gm);
+		for (int i = 0; i < inv.getSize(); i++) {
+			int slot = i + 1;
+			if (plugin.bconfig.getItemStack(player.getUniqueId().toString() + ".slot." + slot) != null) {
+				if (i >= 8) {
+					ItemStack item = new ItemStack(plugin.bconfig.getItemStack(id + ".slot." + slot));
+					inv.setItem(slot, item);
+				} else {
+					ItemStack item = new ItemStack(plugin.bconfig.getItemStack(id + ".slot." + slot));
+					inv.setItem(i, item);
+				}
+			}
+		}
 		inv.setItem(8, gold);
 		player.openInventory(inv);
+	}
+
+	public void goldTransit(Player player) {
+
 	}
 }
--- Bank/src/me/dojmgubben/bank/PlayerListener.java
+++ Bank/src/me/dojmgubben/bank/PlayerListener.java
@@ -5,8 +5,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class PlayerListener implements Listener {
 
@@ -40,6 +43,50 @@ public class PlayerListener implements Listener {
 					}
 				} else {
 					msg.noperm(player);
+				}
+			}
+		}
+	}
+
+	@EventHandler
+	public void onInventoryclick(InventoryClickEvent event) {
+		Player player = (Player) event.getWhoClicked();
+		if (event.getInventory().getTitle().equalsIgnoreCase(player.getName() + "'s Bank")) {
+			event.setCancelled(true);
+			ItemStack item = event.getCurrentItem();
+			ItemMeta im = item.getItemMeta();
+			int slot = event.getRawSlot();
+			if (slot < event.getInventory().getSize()) {
+				slot = slot + 1;
+				if (item.hasItemMeta()) {
+					if (im.getDisplayName().equalsIgnoreCase(plugin.config.getString("bank.gold.name"))) {
+						methods.goldTransit(player);
+					} else {
+						plugin.bconfig.set(player.getUniqueId().toString() + ".slot." + slot, null);
+						plugin.saveb();
+						event.getInventory().remove(item);
+						player.getInventory().addItem(item);
+						msg.msg(player, "taken from bank");
+
+					}
+				} else {
+					plugin.bconfig.set(player.getUniqueId().toString() + ".slot." + slot, null);
+					plugin.saveb();
+					event.getInventory().remove(item);
+					player.getInventory().addItem(item);
+					msg.msg(player, "taken from bank");
+				}
+			} else {
+				for (int i = 0; i < event.getInventory().getSize(); i++) {
+					slot = i + 1;
+					if (plugin.bconfig.getItemStack(player.getUniqueId().toString() + ".slot." + slot) == null) {
+						plugin.bconfig.set(player.getUniqueId().toString() + ".slot." + slot, item);
+						plugin.saveb();
+						msg.msg(player, "added to bank");
+						player.getInventory().remove(item);
+						methods.enterBank(player, plugin.bconfig.getInt(player.getUniqueId().toString() + ".size"));
+						break;
+					}
 				}
 			}
 		}
